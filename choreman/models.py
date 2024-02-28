c9c12a9d2e0bacd07cefcd01fc511fac92d21dc5
Kali <39596472+Space-G@users.noreply.github.com>
2024-02-28 20:21:29 -0300

    Creates User and Chore models

--- choreman/models.py
+++ choreman/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    login_name = models.CharField(primary_key = True, max_length = 254) # max email address length
+    nickname = models.CharField(max_length = 64)
+    registration_datetime = models.DateTimeField()
+
+class Chore(models.Model):
+    owner = models.ForeignKey(User, on_delete = models.CASCADE)
+    is_done = models.BooleanField(default = False)
+    creation_datetime = models.DateTimeField()
+    chore_text = models.CharField(max_length = 256)
