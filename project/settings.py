5138c1c948878c4ffde4315824f82e289dd6715f
Kali <39596472+Space-G@users.noreply.github.com>
2024-03-05 18:07:02 -0300

    Add choreman to the project's installed apps

--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'choreman.apps.ChoremanConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
