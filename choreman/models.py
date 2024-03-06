77be2aa0c1a3b63f1f773067fc8b902cd8875a77
Kali <39596472+Space-G@users.noreply.github.com>
2024-03-06 00:47:41 -0300

    Add __str__ methods to /choreman/models

--- choreman/models.py
+++ choreman/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 
 
@@ -7,8 +9,14 @@ class User(models.Model):
     nickname = models.CharField("name shown", max_length = 64)
     registration_datetime = models.DateTimeField()
 
+    def __str__(self):
+        return f"{self.login_name} {self.nickname} {self.registration_datetime.isoformat(' ')}"
+
 class Chore(models.Model):
     creator = models.ForeignKey(User, on_delete = models.CASCADE)
     is_done = models.BooleanField(default = False)
     creation_datetime = models.DateTimeField()
     chore_description = models.CharField(max_length = 256)
+
+    def __str__(self):
+        return f"{self.creation_datetime.isoformat(' ')}\t{self.creator.login_name} ({self.creator.nickname})\t{'✔' if self.is_done else '❌'}\t{self.chore_description}"
